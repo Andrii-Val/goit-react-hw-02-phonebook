1bdf882e01e55d99566253f7dca8dddf75261867
Andrii-Val <andrii.val1006@gmail.com>
2023-09-08 14:57:25 +0300

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,13 @@
+import { render } from '@testing-library/react';
+import {AddContac} from "./contactItem/ContactItem";
+
 export const App = () => {
+  
+    
   return (
-    <></>
-  );
-};
+    <>
+    <AddContac/>
+    </>
+  )
+ };
+
--- src/components/contactItem/ContactItem.js
+++ src/components/contactItem/ContactItem.js
@@ -1,1 +1,44 @@
-import {Formir, Form, Field} from 'formik'
+import { Formik, Field, Form } from 'formik';
+// import { Container, FormItem } from './NewContact.styled';
+
+export  const AddContact = ({ create }) => {
+    return (
+        <>
+            <h1>Contacts</h1>
+            <Formik
+            initialValues={{
+                name: '',
+                number: '',
+            }}
+            
+            onSubmit={ (values,{resetForm}) => {
+                create(values);
+                resetForm();
+            }}
+            >
+            <Form>
+               
+                    <label htmlFor="Name">Name</label>
+                    <Field id="Name" name="name" placeholder="Jacob Mercer" type="text" required/>
+                
+               
+                    <label htmlFor="Number">Number</label>
+                    <Field 
+                    id="Number" 
+                    name="number" 
+                    placeholder="227-91-26" 
+                    type="tel" 
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    />
+              
+                
+                    <button type="submit">Add contact</button>
+                
+            </Form>
+            </Formik>
+       </>
+
+    );
+}
